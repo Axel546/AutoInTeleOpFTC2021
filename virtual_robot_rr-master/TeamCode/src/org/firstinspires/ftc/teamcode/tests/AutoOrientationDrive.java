6d432b9b74ffa3b99e36a4efa5f47312b307cc71
Axel546 <62812228+Axel546@users.noreply.github.com>
2021-04-10 22:27:46 +0300

    AutoOrientation

---
+++ virtual_robot_rr-master/TeamCode/src/org/firstinspires/ftc/teamcode/tests/AutoOrientationDrive.java
@@ -0,0 +1,132 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.util.Angle;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.teamcode.rr_quickstart_examples.drive.SampleMecanumDriveCancelable;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * This opmode demonstrates how one can augment driver control by following Road Runner arbitrary
+ * Road Runner trajectories at any time during teleop. This really isn't recommended at all. This is
+ * not what Trajectories are meant for. A path follower is more suited for this scenario. This
+ * sample primarily serves as a demo showcasing Road Runner's capabilities.
+ * <p>
+ * This bot starts in driver controlled mode by default. The player is able to drive the bot around
+ * like any teleop opmode. However, if one of the select buttons are pressed, the bot will switch
+ * to automatic control and run to specified location on its own.
+ * <p>
+ * If A is pressed, the bot will generate a splineTo() trajectory on the fly and follow it to
+ * targetA (x: 45, y: 45, heading: 90deg).
+ * <p>
+ * If B is pressed, the bot will generate a lineTo() trajectory on the fly and follow it to
+ * targetB (x: -15, y: 25, heading: whatever the heading is when you press B).
+ * <p>
+ * If Y is pressed, the bot will turn to face 45 degrees, no matter its position on the field.
+ * <p>
+ * Pressing X will cancel trajectory following and switch control to the driver. The bot will also
+ * cede control to the driver once trajectory following is done.
+ * <p>
+ * The following may be a little off with this method as the trajectory follower and turn
+ * function assume the bot starts at rest.
+ * <p>
+ * This sample utilizes the SampleMecanumDriveCancelable.java class.
+ */
+@TeleOp(name = "OrientationAuto", group = "advanced")
+public class AutoOrientationDrive extends LinearOpMode {
+    enum Mode {
+        DRIVER_CONTROL,
+        AUTOMATIC_CONTROL
+    }
+
+    Mode currentMode = Mode.DRIVER_CONTROL;
+
+    // The coordinates of the tower
+    Vector2d towerVector = new Vector2d(120,-6);
+
+    // The angle we want to align to when we press Y
+    double targetAngle = Math.toRadians(0);
+    double distanceToTower = 0;
+
+    @Override
+    public void runOpMode() throws InterruptedException, FileNotFoundException {
+        // Initialize custom cancelable SampleMecanumDrive class
+        SampleMecanumDriveCancelable drive = new SampleMecanumDriveCancelable(hardwareMap);
+
+        // We want to turn off velocity control for teleop
+        // Velocity control per wheel is not necessary outside of motion profiled auto
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Retrieve our pose from the PoseStorage.currentPose static field
+        // See AutoTransferPose.java for further details
+        drive.setPoseEstimate(PoseStorage.currentPose);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive() && !isStopRequested()) {
+            // Update the drive class
+            drive.update();
+
+            // Read pose
+            Pose2d poseEstimate = drive.getPoseEstimate();
+
+            // Print pose to telemetry
+            telemetry.addData("mode", currentMode);
+            telemetry.addData("x", poseEstimate.getX());
+            telemetry.addData("y", poseEstimate.getY());
+            telemetry.addData("heading", poseEstimate.getHeading());
+            telemetry.addData("target",Math.toDegrees(targetAngle));
+            telemetry.addData("distanceToTower",distanceToTower);
+            telemetry.addData("perpendicularDistance",towerVector.getX()-poseEstimate.getX());
+            telemetry.update();
+
+            // We follow different logic based on whether we are in manual driver control or switch
+            // control to the automatic mode
+            switch (currentMode) {
+                case DRIVER_CONTROL:
+                    drive.setDrivePower(
+                            new Pose2d(
+                                    -gamepad1.left_stick_y,
+                                    -gamepad1.left_stick_x,
+                                    -gamepad1.right_stick_x
+                            )
+                    );
+
+                    if (gamepad1.y) {
+                        // If Y is pressed, we turn the bot to the specified angle to reach
+                        // the tower
+
+                        targetAngle = Math.atan2(-poseEstimate.getY() + towerVector.getY(), -poseEstimate.getX() + towerVector.getX());
+
+                        drive.turnAsync(Angle.normDelta(targetAngle - poseEstimate.getHeading()));
+
+                        currentMode = Mode.AUTOMATIC_CONTROL;
+                    }
+                    break;
+                case AUTOMATIC_CONTROL:
+                    // If x is pressed, we break out of the automatic following
+                    if (gamepad1.x) {
+                        drive.cancelFollowing();
+                        currentMode = Mode.DRIVER_CONTROL;
+                    }
+
+                    // If drive finishes its task, cede control to the driver
+                    if (!drive.isBusy()) {
+                        currentMode = Mode.DRIVER_CONTROL;
+                    }
+                    break;
+            }
+        }
+    }
+}
